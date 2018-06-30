e6b0c6a62f96e46667ae1aaf9a2626f7f6632236
=Prakash <=prakash808109@gmail.com>
2018-06-30 12:35:13 +0530

    Commit1

---
+++ SampleProject2/src/Githubsample1/githubintegration.java
@@ -0,0 +1,13 @@
+package Githubsample1;
+
+import org.testng.annotations.Test;
+
+public class githubintegration {
+
+	@Test
+	public void sample() {
+		System.out.println("Integrated successfully");
+	}
+	
+
+}
